b3b721a79cbb67ef592afa7b6cee55ed097b462f
ashoordoushow <ashoor12323@gmail.com>
2024-10-30 12:46:07 -0500

    fitness tracker Vue.js frontend CRUD

---
+++ src/components/Content.vue
@@ -0,0 +1,26 @@
+<script>
+import ExercisesIndex from "./ExercisesIndex.vue";
+
+export default {
+  components: {
+    ExercisesIndex,
+  },
+  data: function () {
+    return {
+      UNFINISHED INDEX ACTION BELOW FROM VUE FRONTEND CRUD GUIDE
+      exercises: [ 
+        { id: 1, workout_id: name: "Bench", reps: image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgy5RWhaieyrevjvS6ErgThrTBiYQL44cfTA&s"}
+        { id: 2, }
+      ]
+    }
+  }
+};
+</script>
+
+<template>
+  <main>
+    <ExercisesIndex />
+  </main>
+</template>
+
+<style></style>
---
+++ src/components/ExercisesIndex.vue
@@ -0,0 +1,9 @@
+<script></script>
+
+<template>
+  <div>
+    <h1>All exercises</h1>
+  </div>
+</template>
+
+<style></style>
