c218e92ef6130f679f553b0b165aa316a310ea36
ashoordoushow <ashoor12323@gmail.com>
2024-10-30 17:40:54 -0500

    create action Vue

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,10 +1,12 @@
 <script>
 import axios from "axios";
 import ExercisesIndex from "./ExercisesIndex.vue";
+import ExercisesNew from "./ExercisesNew.vue";
 
 export default {
   components: {
     ExercisesIndex,
+    ExercisesNew
   },
   data: function () {
     return {
@@ -21,18 +23,27 @@ export default {
         this.exercises = response.data;
       });
     },
+    handleCreateExercise: function (params) {
+      axios
+        .post("http://localhost:3000/exercises.json", params)
+        .then((response) => {
+          console.log("exercise create", response);
+          this.exercises.push(response.data);
+        })
+        .catch((error) => {
+          console.log("exercises create error", error.response);
+        });
+    },
   },
 };
 </script>
 
 <template>
   <main>
+    <ExercisesNew v-on:createExercise="handleCreateExercise" />
     <ExercisesIndex v-bind:exercises="exercises" />
   </main>
 </template>
 
 <style></style>
 
-
-<!-- { id: 1, workout_id: 1, name: "Bench", reps: 10, sets: "3 sets of 10", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgy5RWhaieyrevjvS6ErgThrTBiYQL44cfTA&s"},
-        { id: 2, workout_id: 2, name: "Squat", reps: 5, sets: "3 sets of 5", image_url: "https://www.lifefitness.com/Kentico13CoreBase/media/LFMedia/LifeFitnessImages/MediaSync/332-687-hammerstrenght-olympic-squat-rack-l.png?h=630&w=1200"} -->
---
+++ src/components/ExercisesNew.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+  data: function () {
+    return {
+      newExerciseParams: {},
+    };
+  },
+  methods: {
+    handleSubmit: function () {
+      this.$emit("createExercise", this.newExerciseParams);
+      this.newExerciseParams = {};
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>New Exercise</h1>
+    <form v-on:submit.prevent="handleSubmit">
+      <div>
+        Name:
+        <input type="text" v-model="newExerciseParams.name" />
+      </div>
+      <div>
+        Image_url:
+        <input type="text" v-model="newExerciseParams.image_url" />
+      </div>
+      <div>
+        Workout_id:
+        <input type="text" v-model="newExerciseParams.workout_id" />
+      </div>
+      <div>
+        Reps:
+        <input type="text" v-model="newExerciseParams.reps" />
+      </div>
+      <div>
+        Sets:
+        <input type="text" v-model="newExerciseParams.sets" />
+      </div>
+      <button type="submit">Create exercise</button>
+    </form>
+  </div>
+</template>
+
+<style></style>
