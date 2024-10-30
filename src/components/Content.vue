9cd9eb6e75ec2d2e9b4d9803bab1cb91b1ff58e3
ashoordoushow <ashoor12323@gmail.com>
2024-10-30 17:04:52 -0500

    fitness tracker frontend index action

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,4 +1,5 @@
 <script>
+import axios from "axios";
 import ExercisesIndex from "./ExercisesIndex.vue";
 
 export default {
@@ -7,20 +8,31 @@ export default {
   },
   data: function () {
     return {
-      UNFINISHED INDEX ACTION BELOW FROM VUE FRONTEND CRUD GUIDE
-      exercises: [ 
-        { id: 1, workout_id: name: "Bench", reps: image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgy5RWhaieyrevjvS6ErgThrTBiYQL44cfTA&s"}
-        { id: 2, }
-      ]
-    }
-  }
+      exercises: [],
+    };
+  },
+  created: function () {
+    this.handleIndexExercises();
+  },
+  methods: {
+    handleIndexExercises: function () {
+      axios.get("http://localhost:3000/exercises.json").then((response) => {
+        console.log("exercises index", response);
+        this.exercises = response.data;
+      });
+    },
+  },
 };
 </script>
 
 <template>
   <main>
-    <ExercisesIndex />
+    <ExercisesIndex v-bind:exercises="exercises" />
   </main>
 </template>
 
 <style></style>
+
+
+<!-- { id: 1, workout_id: 1, name: "Bench", reps: 10, sets: "3 sets of 10", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgy5RWhaieyrevjvS6ErgThrTBiYQL44cfTA&s"},
+        { id: 2, workout_id: 2, name: "Squat", reps: 5, sets: "3 sets of 5", image_url: "https://www.lifefitness.com/Kentico13CoreBase/media/LFMedia/LifeFitnessImages/MediaSync/332-687-hammerstrenght-olympic-squat-rack-l.png?h=630&w=1200"} -->
--- src/components/ExercisesIndex.vue
+++ src/components/ExercisesIndex.vue
@@ -1,8 +1,21 @@
-<script></script>
+<script>
+export default {
+  props: {
+    exercises: Array,
+  },
+};
+</script>
 
 <template>
   <div>
     <h1>All exercises</h1>
+    <div v-for="exercise in exercises" v-bind:key="exercise.id">
+      <h2>{{  exercise.name }}</h2>
+      <img v-bind:src="exercise.image_url" />
+      <p>Reps: {{ exercise.reps }}</p>
+      <p>Sets: {{ exercise.sets }}</p>
+      <p>Workout_id: {{ exercise.workout_id }}</p>
+    </div>
   </div>
 </template>
 
